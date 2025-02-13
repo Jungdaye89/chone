cb24b041a2f425e041fa076ff36ffa1255b14878
KIM SOOBIN <xpsxm225@naver.com>
2025-02-13 17:18:53 +0900

    Init: Project Init

---
+++ src/main/java/com/chone/server/ChoneApplication.java
@@ -0,0 +1,13 @@
+package com.chone.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoneApplication.class, args);
+    }
+
+}
