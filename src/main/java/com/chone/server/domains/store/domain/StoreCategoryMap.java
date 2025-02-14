b9b45466841b4b9175873515d59eb8107394575f
Goldbar97 <ehrm1357@gmail.com>
2025-02-14 13:00:14 +0900

    Feat: Store, Product Entity 추가

---
+++ src/main/java/com/chone/server/domains/store/domain/StoreCategoryMap.java
@@ -0,0 +1,41 @@
+package com.chone.server.domains.store.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+@AllArgsConstructor
+@Builder
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "p_store_category_map")
+public class StoreCategoryMap {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @JdbcTypeCode(SqlTypes.UUID)
+  private UUID id;
+
+  @NotNull
+  @ManyToOne
+  @JoinColumn(name = "store_id", nullable = false)
+  private Store store;
+
+  @NotNull
+  @ManyToOne
+  @JoinColumn(name = "category_id", nullable = false)
+  private Category category;
+}
