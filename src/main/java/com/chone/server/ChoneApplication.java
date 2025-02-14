c0c73a3fa3648ddfe804f42ca7bbe6baa5c30d25
Goldbar97 <ehrm1357@gmail.com>
2025-02-14 11:25:07 +0900

    Feat: BaseEntity 생성 및 추가

--- src/main/java/com/chone/server/ChoneApplication.java
+++ src/main/java/com/chone/server/ChoneApplication.java
@@ -2,8 +2,10 @@ package com.chone.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ChoneApplication {
 
     public static void main(String[] args) {
