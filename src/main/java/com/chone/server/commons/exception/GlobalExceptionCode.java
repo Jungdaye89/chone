ab738461035212a080aca77842544df6fb35ffa3
Goldbar97 <ehrm1357@gmail.com>
2025-02-16 16:58:41 +0900

    Update: ExceptionCode 수정

--- src/main/java/com/chone/server/commons/exception/GlobalExceptionCode.java
+++ src/main/java/com/chone/server/commons/exception/GlobalExceptionCode.java
@@ -2,7 +2,6 @@ package com.chone.server.commons.exception;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,11 +10,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum GlobalExceptionCode implements ExceptionCode {
-  USER_NOT_FOUND(NOT_FOUND, "존재하지 않는 사용자입니다."),
-  CATEGORY_NOT_FOUND(NOT_FOUND, "존재하지 않는 카테고리입니다."),
-  LEGAL_DONG_NOT_FOUND(NOT_FOUND, "존재하지 않는 법정동입니다."),
-  STORE_NOT_FOUND(NOT_FOUND, "존재하지 않는 가게입니다."),
-  USER_OWNED_STORE_NOT_FOUND(NOT_FOUND, "사용자가 소유한 가게가 없습니다."),
+
   INTERNAL_ERROR(INTERNAL_SERVER_ERROR, "서버 내부 오류로 인해 요청을 처리할 수 없습니다."),
   INVALID_INPUT(BAD_REQUEST, "입력하신 데이터에 오류가 있습니다. 요청 내용을 확인하고 다시 시도해 주세요."),
   NOT_READABLE(BAD_REQUEST, "입력하신 데이터가 잘못된 형식입니다."),
