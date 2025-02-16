09bf19bbd3f5e9a9a6af6542937aaa52337c7d71
KIM SOOBIN <xpsxm225@naver.com>
2025-02-17 00:45:00 +0900

    Feat: 인증 및 인가 관련 에러를 Global Exception Handler에 정의
    AccessDeniedException
    - GlobalExceptionCode에 FORBIDDEN로 정의
    AuthenticationException
    - GlobalExceptionCode에 UNAUTHORIZED로 정의
    
    Resolved #18

--- src/main/java/com/chone/server/commons/exception/GlobalExceptionCode.java
+++ src/main/java/com/chone/server/commons/exception/GlobalExceptionCode.java
@@ -14,7 +14,9 @@ public enum GlobalExceptionCode implements ExceptionCode {
   INTERNAL_ERROR(INTERNAL_SERVER_ERROR, "서버 내부 오류로 인해 요청을 처리할 수 없습니다."),
   INVALID_INPUT(BAD_REQUEST, "입력하신 데이터에 오류가 있습니다. 요청 내용을 확인하고 다시 시도해 주세요."),
   NOT_READABLE(BAD_REQUEST, "입력하신 데이터가 잘못된 형식입니다."),
-  INVALID_REQUEST(BAD_REQUEST, "요청하신 값이 올바르지 않습니다. 요청 값을 확인해 주세요.");
+  INVALID_REQUEST(BAD_REQUEST, "요청하신 값이 올바르지 않습니다. 요청 값을 확인해 주세요."),
+  FORBIDDEN(HttpStatus.FORBIDDEN, "요청한 리소스에 대한 권한이 없습니다."),
+  UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "로그인이 필요한 서비스입니다. 로그인 후 다시 시도해주세요.");
 
   private final HttpStatus status;
   private final String message;
