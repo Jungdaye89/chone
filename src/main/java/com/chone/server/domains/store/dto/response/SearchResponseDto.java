a9937a4ef426a276134508061af7831a0c5bcebc
Goldbar97 <ehrm1357@gmail.com>
2025-02-16 17:30:24 +0900

    Update: private 제어자 추가

--- src/main/java/com/chone/server/domains/store/dto/response/SearchResponseDto.java
+++ src/main/java/com/chone/server/domains/store/dto/response/SearchResponseDto.java
@@ -12,6 +12,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SearchResponseDto {
 
-  List<ReadResponseDto> content;
+  private List<ReadResponseDto> content;
   private PageInfoDto pageInfo;
 }
