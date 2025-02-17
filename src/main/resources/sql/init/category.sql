abfe5e96b9f656ce0445abb109b6e63f58cdbf3a
Goldbar97 <ehrm1357@gmail.com>
2025-02-18 00:20:01 +0900

    Feat: category sql 파일 생성

---
+++ src/main/resources/sql/init/category.sql
@@ -0,0 +1,4 @@
+INSERT INTO p_category (id, name, is_available, created_at, created_by)
+VALUES ('9e1c7ba4-d3ce-42c2-b03b-f0414ebe9cb8', '한식', true, now(), 'system'),
+       ('4f9fd274-7fe3-48a2-b773-b93c08be3cdb', '중식', true, now(), 'system'),
+       ('428bd994-2c31-4a2c-b78e-d16a09988c8b', '일식', true, now(), 'system');
