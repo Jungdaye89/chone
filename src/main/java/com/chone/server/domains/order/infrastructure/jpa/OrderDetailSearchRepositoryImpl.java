3c37dde2ca735f97765544a41c70bb1e4245c52e
KIM SOOBIN <xpsxm225@naver.com>
2025-02-18 23:11:21 +0900

    Feat: OrderDetailSearchRepository 구현체 개요 작성
    역할별 주문 조회 메서드 기본 구조 구현했습니다.
    
    findOrderDetailByIdForCustomer
    - 고객(Customer), 소유자 권한 검증 로직 필요
    - ORDER_CUSTOMER_ACCESS_DENIED 에러 처리
    
    findOrderDetailByIdForOwner
    - 가게 주인(Owner), 가게의 주문인지 권한 검증 로직 필요
    - ORDER_STORE_OWNER_ACCESS_DENIED 에러 처리
    
    findOrderDetailByIdForAdmin
    - 관리자(Admin), 모든 상세 조회 접근 가능
    
    findOrderDetailById
    공통 조회 로직을 위한 메서드
    
    Related to: #51

--- src/main/java/com/chone/server/domains/order/infrastructure/jpa/OrderDetailSearchRepositoryImpl.java
+++ src/main/java/com/chone/server/domains/order/infrastructure/jpa/OrderDetailSearchRepositoryImpl.java
@@ -1,7 +1,12 @@
 package com.chone.server.domains.order.infrastructure.jpa;
 
+import static com.chone.server.domains.store.domain.QStore.store;
+
+import com.chone.server.commons.exception.ApiBusinessException;
 import com.chone.server.domains.order.dto.response.OrderDetailResponse;
+import com.chone.server.domains.order.exception.OrderExceptionCode;
 import com.chone.server.domains.order.repository.OrderDetailSearchRepository;
+import com.chone.server.domains.store.domain.Store;
 import com.chone.server.domains.user.domain.User;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.UUID;
@@ -15,16 +20,38 @@ public class OrderDetailSearchRepositoryImpl implements OrderDetailSearchReposit
 
   @Override
   public OrderDetailResponse findOrderDetailByIdForCustomer(UUID orderId, User customerUser) {
-    return null;
+    OrderDetailResponse response = findOrderDetailById(orderId);
+
+    if (!customerUser.getId().equals(response.user().id())) {
+      throw new ApiBusinessException(OrderExceptionCode.ORDER_CUSTOMER_ACCESS_DENIED);
+    }
+
+    return response;
   }
 
   @Override
-  public OrderDetailResponse findOrderDetailByIdForOwner(UUID orderId, User customerUser) {
-    return null;
+  public OrderDetailResponse findOrderDetailByIdForOwner(UUID orderId, User ownerUser) {
+    OrderDetailResponse response = findOrderDetailById(orderId);
+
+    Store userStore =
+        queryFactory
+            .selectFrom(store)
+            .where(store.id.eq(response.store().id()), store.user.id.eq(ownerUser.getId()))
+            .fetchOne();
+
+    if (userStore == null) {
+      throw new ApiBusinessException(OrderExceptionCode.ORDER_STORE_OWNER_ACCESS_DENIED);
+    }
+
+    return response;
   }
 
   @Override
   public OrderDetailResponse findOrderDetailByIdForAdmin(UUID orderId) {
+    return findOrderDetailById(orderId);
+  }
+
+  private OrderDetailResponse findOrderDetailById(UUID orderId) {
     return null;
   }
 }
