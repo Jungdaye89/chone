317c4fa40550aa0aa0696b7253d0313055c9569e
KIM SOOBIN <xpsxm225@naver.com>
2025-02-18 23:14:04 +0900

    Feat: 주문 상세 조회를 위한 메서드 개요 작성
    findOrderDetailById 메서드 내용
    - 주요 주문 데이터와 주문 아이템 데이터를 분리하여 조회하는 구조
    - 주문이 존재하지 않을 경우 비즈니스 예외 처리
    - fetchOrderMainData: 주문, 사용자, 매장 기본 정보 조회
     - fetchOrderItems: 주문에 포함된 상품 항목 정보 조회
    - 결과 데이터 변환 및 조합을 반환(OrderDetailResponse)
    
    Related to: #51

--- src/main/java/com/chone/server/domains/order/infrastructure/jpa/OrderDetailSearchRepositoryImpl.java
+++ src/main/java/com/chone/server/domains/order/infrastructure/jpa/OrderDetailSearchRepositoryImpl.java
@@ -4,11 +4,14 @@ import static com.chone.server.domains.store.domain.QStore.store;
 
 import com.chone.server.commons.exception.ApiBusinessException;
 import com.chone.server.domains.order.dto.response.OrderDetailResponse;
+import com.chone.server.domains.order.dto.response.OrderDetailResponse.OrderItemResponse;
 import com.chone.server.domains.order.exception.OrderExceptionCode;
 import com.chone.server.domains.order.repository.OrderDetailSearchRepository;
 import com.chone.server.domains.store.domain.Store;
 import com.chone.server.domains.user.domain.User;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -52,6 +55,31 @@ public class OrderDetailSearchRepositoryImpl implements OrderDetailSearchReposit
   }
 
   private OrderDetailResponse findOrderDetailById(UUID orderId) {
+
+    Tuple mainData = fetchOrderMainData(orderId);
+
+    if (mainData == null) {
+      throw new ApiBusinessException(OrderExceptionCode.NOT_FOUND_ORDER);
+    }
+
+    OrderDetailResponse.OrderResponse orderResponseData =
+        mainData.get(0, OrderDetailResponse.OrderResponse.class);
+    OrderDetailResponse.UserResponse userResponseData =
+        mainData.get(1, OrderDetailResponse.UserResponse.class);
+    OrderDetailResponse.StoreResponse storeResponseData =
+        mainData.get(2, OrderDetailResponse.StoreResponse.class);
+
+    List<OrderItemResponse> orderItems = fetchOrderItems(orderId);
+
+    return new OrderDetailResponse(
+        orderResponseData, userResponseData, storeResponseData, orderItems);
+  }
+
+  private Tuple fetchOrderMainData(UUID orderId) {
+    return null;
+  }
+
+  private List<OrderItemResponse> fetchOrderItems(UUID orderId) {
     return null;
   }
 }
