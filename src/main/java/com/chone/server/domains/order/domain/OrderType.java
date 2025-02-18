c42610c6558a124848feb93d8b6f048244494d5c
KIM SOOBIN <xpsxm225@naver.com>
2025-02-18 11:55:59 +0900

    Update: 주문 생성 Response Body Dto 수정

--- src/main/java/com/chone/server/domains/order/domain/OrderType.java
+++ src/main/java/com/chone/server/domains/order/domain/OrderType.java
@@ -1,6 +1,13 @@
 package com.chone.server.domains.order.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum OrderType {
-  ONLINE,
-  OFFLINE
+  ONLINE("배달"),
+  OFFLINE("대면");
+
+  private final String description;
 }
--- src/main/java/com/chone/server/domains/order/dto/response/CreateOrderResponse.java
+++ src/main/java/com/chone/server/domains/order/dto/response/CreateOrderResponse.java
@@ -1,16 +1,40 @@
 package com.chone.server.domains.order.dto.response;
 
 import com.chone.server.domains.order.domain.Order;
-import com.chone.server.domains.order.domain.OrderStatus;
+import com.chone.server.domains.order.domain.OrderItem;
+import com.chone.server.domains.store.domain.Store;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
-public record CreateOrderResponse(UUID orderId, OrderStatus status, LocalDateTime createdAt) {
-  private CreateOrderResponse(Order order) {
-    this(order.getId(), order.getStatus(), order.getCreatedAt());
+public record CreateOrderResponse(
+    OrderResponse order, StoreResponse store, List<OrderItemResponse> orderItem) {
+
+  private CreateOrderResponse(Order order, Store store, List<OrderItem> orderItems) {
+    this(
+        new OrderResponse(
+            order.getId(),
+            order.getTotalPrice().intValue(),
+            order.getOrderType().getDescription(),
+            order.getStatus().getDescription(),
+            order.getCreatedAt()),
+        new StoreResponse(store.getName(), store.getAddress()),
+        orderItems.stream()
+            .map(
+                orderItem ->
+                    new OrderItemResponse(orderItem.getProduct().getName(), orderItem.getAmount()))
+            .collect(Collectors.toList()));
   }
 
   public static CreateOrderResponse from(Order order) {
-    return new CreateOrderResponse(order);
+    return new CreateOrderResponse(order, order.getStore(), order.getOrderItems());
   }
+
+  private record OrderResponse(
+      UUID orderId, int totalPrice, String orderType, String status, LocalDateTime createdAt) {}
+
+  private record StoreResponse(String name, String address) {}
+
+  public record OrderItemResponse(String name, int amount) {}
 }
