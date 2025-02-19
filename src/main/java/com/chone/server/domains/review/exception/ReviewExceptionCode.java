eb462fe6c6d99229f696a0458ab73ccf5ba3758e
Jungdaye89 <jungyj0809@gmail.com>
2025-02-19 14:35:49 +0900

    Feat: Review 예외 코드 추가
    - 존재하지 않는 리뷰 예외 처리 (REVIEW_NOT_FOUND)
    - 비공개 리뷰 접근 제한 (REVIEW_ACCESS_DENIED)
    
    Resolved #43

--- src/main/java/com/chone/server/domains/review/exception/ReviewExceptionCode.java
+++ src/main/java/com/chone/server/domains/review/exception/ReviewExceptionCode.java
@@ -18,7 +18,9 @@ public enum ReviewExceptionCode implements ExceptionCode {
   STORE_NOT_FOUND(NOT_FOUND, "해당 가게를 찾을 수 없습니다."),
   REVIEW_ALREADY_EXISTS(CONFLICT, "해당 주문에 대한 리뷰가 이미 존재합니다."),
   ORDER_NOT_COMPLETED(UNPROCESSABLE_ENTITY, "완료된 주문에 대해서만 리뷰를 작성할 수 있습니다."),
-  FILE_UPLOAD_ERROR(INTERNAL_SERVER_ERROR, "이미지 업로드 중 오류가 발생했습니다.");
+  FILE_UPLOAD_ERROR(INTERNAL_SERVER_ERROR, "이미지 업로드 중 오류가 발생했습니다."),
+  REVIEW_ACCESS_DENIED(FORBIDDEN, "해당 요청을 수행할 권한이 없습니다."),
+  REVIEW_NOT_FOUND(NOT_FOUND, "요청하신 리뷰 정보를 찾을 수 없습니다.");
 
   private final HttpStatus status;
   private final String message;
