d1cdedd5c986c6aec8c94a795effde570cd4af78
s_nowsssㄴㄴㄴㄴsassssjgi2447a <howlarang123@naver.com>
2025-02-19 17:16:31 +0900

    Chore: 어노테이션 변경
    @Data 삭제
    @Getter 추가
    
    Resolved: #56

--- src/main/java/com/chone/server/domains/user/dto/request/LoginRequestDto.java
+++ src/main/java/com/chone/server/domains/user/dto/request/LoginRequestDto.java
@@ -2,11 +2,10 @@ package com.chone.server.domains.user.dto.request;
 
 import lombok.*;
 
-@Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequestDto {
-
     private String username;
     private String password;
 }
