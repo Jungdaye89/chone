2bdf6cb7211ef4022544b9adedc2607a83f29f78
s_nowsssㄴㄴㄴㄴsassssjgi2447a <howlarang123@naver.com>
2025-02-19 15:21:49 +0900

    Refactor: Repository findByEmail 추가
    이메일 중복체크를 위한 findByEmail 추가
    
    Resolved #56

--- src/main/java/com/chone/server/domains/user/repository/UserRepository.java
+++ src/main/java/com/chone/server/domains/user/repository/UserRepository.java
@@ -4,11 +4,13 @@ import com.chone.server.domains.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByUsername(String username);
 
     Optional<User> findById(Long userId);
+
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
 }
