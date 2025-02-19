150b6c4be75e14e34274c3eb170dbfb0e0fec551
s_nowsssㄴㄴㄴㄴsassssjgi2447a <howlarang123@naver.com>
2025-02-19 14:53:04 +0900

    Refactor: CustomUserDetailsService 변경
    CustomUserDetailsService 수정
    
    Resolved #16

--- src/main/java/com/chone/server/domains/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/chone/server/domains/auth/service/CustomUserDetailsService.java
@@ -3,29 +3,24 @@ package com.chone.server.domains.auth.service;
 import com.chone.server.domains.auth.dto.CustomUserDetails;
 import com.chone.server.domains.user.domain.User;
 import com.chone.server.domains.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-
-    private final UserRepository  userRepository; //사용자 정보를 조회하기 위한 Repository
-
-    public CustomUserDetailsService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final UserRepository userRepository; //사용자 정보를 조회하기 위한 Repository
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        //DB에서 조회
-        User userData = userRepository.findByUsername(username).orElseThrow();
-        if(userData != null) {
-            //UserDetails에 담아서 return하면 AutenticationManager가 검증 함
-            return new CustomUserDetails(userData);
-        }
-        return null;
+        //DB에서 조회 없으면 예외처리
+        User userData = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
+
+        return new CustomUserDetails(userData);
     }
 }
