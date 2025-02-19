4039d4a0f440dc4cae17e711dd22c517d32f9400
s_nowsssㄴㄴㄴㄴsassssjgi2447a <howlarang123@naver.com>
2025-02-19 23:55:06 +0900

    Refactor: successfulAuthentication()로 변경
    로그인 성공하면 response의 body에 반환값 보이도록 설정
    
    Resolved: #56

--- src/main/java/com/chone/server/domains/auth/filter/LoginFilter.java
+++ src/main/java/com/chone/server/domains/auth/filter/LoginFilter.java
@@ -4,6 +4,7 @@ import com.chone.server.commons.jwt.JwtUtil;
 import com.chone.server.domains.auth.dto.CustomUserDetails;
 import com.chone.server.domains.user.domain.Role;
 import com.chone.server.domains.user.dto.request.LoginRequestDto;
+import com.chone.server.domains.user.dto.response.LoginResponseDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -47,12 +48,16 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     //로그인 성공시 실행하는 메서드(Jwt 발급)
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) {
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
         String username = ((CustomUserDetails) authResult.getPrincipal()).getUsername();
         Role role = ((CustomUserDetails) authResult.getPrincipal()).getUser().getRole();
 
         String token = jwtUtil.createToken(username, role);
         response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().write(new ObjectMapper().writeValueAsString(new LoginResponseDto(username,token)));
     }
 
     //로그인 실패시 실행 메서드
