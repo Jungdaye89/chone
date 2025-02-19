a8fed6e117fdf68e67a3c2075c5763803981d2f5
s_nowsssㄴㄴㄴㄴsassssjgi2447a <howlarang123@naver.com>
2025-02-19 14:43:07 +0900

    Refactor: LoginFilter 변경
    LoginFilter 성공한 후 로직 변경
    
    Resolved #16

--- src/main/java/com/chone/server/domains/auth/filter/LoginFilter.java
+++ src/main/java/com/chone/server/domains/auth/filter/LoginFilter.java
@@ -2,85 +2,60 @@ package com.chone.server.domains.auth.filter;
 
 import com.chone.server.commons.jwt.JwtUtil;
 import com.chone.server.domains.auth.dto.CustomUserDetails;
-import com.chone.server.domains.auth.dto.LoginDto;
+import com.chone.server.domains.user.domain.Role;
+import com.chone.server.domains.user.dto.request.LoginRequestDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.authentication.AuthenticationManager;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.util.StreamUtils;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Collection;
-import java.util.Iterator;
 
+@Slf4j(topic = "로그인 및 JWT 생성")
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
-    private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
 
-    public LoginFilter(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
-        this.authenticationManager = authenticationManager;
+    public LoginFilter(JwtUtil jwtUtil) {
         this.jwtUtil = jwtUtil;
+        setFilterProcessesUrl("/api/v1/users/login");
     }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        LoginDto loginDto = new LoginDto();
+        try {
+            LoginRequestDto requestDto = new ObjectMapper().readValue(request.getInputStream(), LoginRequestDto.class);
 
-        try{
-            ObjectMapper mapper = new ObjectMapper();
-            ServletInputStream inputStream = request.getInputStream();
-            String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
-            loginDto = mapper.readValue(messageBody, LoginDto.class);
-        } catch (IOException e){
-            throw new RuntimeException(e);
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            requestDto.getUsername(),
+                            requestDto.getPassword(),
+                            null
+                    )
+            );
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e.getMessage());
         }
-
-        String username = loginDto.getUsername();
-        String password = loginDto.getPassword();
-
-        //스프링 시큐리티에서 username과 password를 검증하기 위해서는 token에 담아야 함
-        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null);
-
-        //token에 담은 검증을 위한 AuthenticationManager로 전달
-        return authenticationManager.authenticate(authToken);
     }
 
     //로그인 성공시 실행하는 메서드(Jwt 발급)
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        CustomUserDetails customUserDetails = (CustomUserDetails) authResult.getPrincipal();
-        String username = customUserDetails.getUsername();
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) {
+        String username = ((CustomUserDetails) authResult.getPrincipal()).getUsername();
+        Role role = ((CustomUserDetails) authResult.getPrincipal()).getUser().getRole();
 
-        Collection<? extends GrantedAuthority> authorities = authResult.getAuthorities();
-        Iterator<? extends GrantedAuthority> authoritiesIterator = authorities.iterator();
-        GrantedAuthority authority = authoritiesIterator.next();
-
-        String role = authority.getAuthority();
-
-        String token = jwtUtil.createToken(username, role, 60*60*10L);
-
-        /*
-        HTTP 인증 방식은 RFC 7235 정의에 따라 아래 인증 헤더 형태를 가져야 한다.
-        Authorization: 타입 인증토큰
-
-        //예시
-        Authorization: Bearer 인증토큰string
-         */
-        response.addHeader("Authorization", "Bearer " + token);
+        String token = jwtUtil.createToken(username, role);
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
     }
 
     //로그인 실패시 실행 메서드
-
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
         response.setStatus(401);
