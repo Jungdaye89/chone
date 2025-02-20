0b16225c97bd8d2be6ef065f8f1e1c27bb4b80bd
s_nowsssㄴㄴㄴㄴsassssjgi2447a <howlarang123@naver.com>
2025-02-20 11:51:18 +0900

    Chore: 회원가입 제약조건 추가
    아이디, 비밀번호, 이메일 제약 조건 추가
    
    Resolved: #56

--- src/main/java/com/chone/server/domains/user/dto/request/SignupRequestDto.java
+++ src/main/java/com/chone/server/domains/user/dto/request/SignupRequestDto.java
@@ -4,18 +4,29 @@ import com.chone.server.domains.user.domain.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignupRequestDto {
+
+    @NotBlank(message = "사용자명을 입력해주세요.")
+    @Size(min = 4, max = 10, message = "사용자명은 4~10자여야 합니다.")
+    @Pattern(regexp = "^[a-z0-9]+$", message = "사용자명은 소문자와 숫자로만 이루어져야 합니다.")
     private String username;
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min = 8, max = 15, message = "비밀번호는 8~15자여야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}|;:'\",.<>?/]).{8,15}$",
+            message = "비밀번호는 대소문자, 숫자, 특수문자를 포함해야 합니다."
+    )
     private String password;
 
-    @NotBlank(message = "이메일 주소를 입력해주세요")
+    @NotBlank(message = "이메일 주소를 입력해주세요.")
     @Email(message = "올바른 형식의 이메일 주소를 입력해주세요.")
     private String email;
 
