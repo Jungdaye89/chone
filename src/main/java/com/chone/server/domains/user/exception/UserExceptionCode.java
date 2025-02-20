ae5eacc01a60b902a6fae1607f5e78d2f1ea7583
s_nowsssㄴㄴㄴㄴsassssjgi2447a <howlarang123@naver.com>
2025-02-20 09:41:14 +0900

    Refactor: 에러코드 추가
    중복된 아이디, 로그인 불가능, 탈퇴한 회원 에러코드 추가
    
    Resolved: #56

--- src/main/java/com/chone/server/domains/user/exception/UserExceptionCode.java
+++ src/main/java/com/chone/server/domains/user/exception/UserExceptionCode.java
@@ -14,8 +14,12 @@ public enum UserExceptionCode implements ExceptionCode {
 
     // 회원
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
+    USER_EXIST_USERNAME(HttpStatus.BAD_REQUEST, "중복된 아이디 입니다."),
     USER_EXIST_EMAIL(HttpStatus.BAD_REQUEST, "중복된 Email입니다."),
-    USER_NOT_AUTHORITY(HttpStatus.BAD_REQUEST,"권한이 없습니다.");
+    USER_NOT_AUTHORITY(HttpStatus.BAD_REQUEST,"권한이 없습니다."),
+    USER_CANT_LOGIN(HttpStatus.BAD_REQUEST, "사용할 수 없는 계정입니다."),
+    USER_DELETED(HttpStatus.BAD_REQUEST, "탈퇴한 회원입니다.");
+
 
 
     // 상태, 메시지, 에러코드
