a8ed672e2edc99ba0714026e4b0faba3f0ece566
KIM SOOBIN <xpsxm225@naver.com>
2025-02-21 00:24:42 +0900

    Update: PaymentRepository 필요한 조회 메서드 추가
    Resolved #62

--- src/main/java/com/chone/server/domains/payment/infrastructure/jpa/PaymentRepositoryImpl.java
+++ src/main/java/com/chone/server/domains/payment/infrastructure/jpa/PaymentRepositoryImpl.java
@@ -1,5 +1,8 @@
 package com.chone.server.domains.payment.infrastructure.jpa;
 
+import com.chone.server.commons.exception.ApiBusinessException;
+import com.chone.server.domains.payment.domain.Payment;
+import com.chone.server.domains.payment.exception.PaymentExceptionCode;
 import com.chone.server.domains.payment.repository.PaymentRepository;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +15,25 @@ public class PaymentRepositoryImpl implements PaymentRepository {
 
   @Override
   public boolean existsByOrderId(UUID id) {
-    return jpaRepository.existsByOrderId(id);
+    return jpaRepository.existsByOrderIdAndDeletedAtNull(id);
+  }
+
+  @Override
+  public Payment save(Payment payment) {
+    return jpaRepository.save(payment);
+  }
+
+  @Override
+  public Payment findById(UUID id) {
+    return jpaRepository
+        .findByIdAndDeletedAtNull(id)
+        .orElseThrow(() -> new ApiBusinessException(PaymentExceptionCode.NOT_FOUND_PAYMENT));
+  }
+
+  @Override
+  public Payment findByOrderId(UUID orderId) {
+    return jpaRepository
+        .findByOrderIdAndDeletedAtNull(orderId)
+        .orElseThrow(() -> new ApiBusinessException(PaymentExceptionCode.NOT_FOUND_PAYMENT));
   }
 }
