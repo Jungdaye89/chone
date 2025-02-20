cdef61e2484172d2dc3d240bf5a851d961d4a977
Goldbar97 <ehrm1357@gmail.com>
2025-02-20 16:16:39 +0900

    Update: S3 서비스와 연동하여 이미지 등록 추가

--- src/main/java/com/chone/server/domains/product/service/ProductService.java
+++ src/main/java/com/chone/server/domains/product/service/ProductService.java
@@ -11,6 +11,7 @@ import com.chone.server.domains.product.dto.response.ReadResponseDto;
 import com.chone.server.domains.product.dto.response.SearchResponseDto;
 import com.chone.server.domains.product.exception.ProductExceptionCode;
 import com.chone.server.domains.product.repository.ProductRepository;
+import com.chone.server.domains.s3.service.S3Service;
 import com.chone.server.domains.store.domain.Store;
 import com.chone.server.domains.store.exception.StoreExceptionCode;
 import com.chone.server.domains.store.service.StoreService;
@@ -24,6 +25,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -33,6 +35,7 @@ public class ProductService {
   private final ProductRepository productRepository;
   private final UserRepository userRepository;
   private final StoreService storeService;
+  private final S3Service s3Service;
 
   public List<Product> findAllById(List<UUID> productIds) {
 
@@ -41,12 +44,16 @@ public class ProductService {
 
   @Transactional
   public CreateResponseDto createProduct(
-      CustomUserDetails userDetails, CreateRequestDto createRequestDto) {
+      CustomUserDetails userDetails, CreateRequestDto createRequestDto, MultipartFile file) {
 
     User user = findUserById(userDetails.getUser().getId());
     Store store = storeService.findStoreById(createRequestDto.getStoreId());
 
     storeService.checkRoleWithStore(user, store);
+
+    if (!file.isEmpty()) {
+      createRequestDto.setImageUrl(s3Service.uploadFile(file));
+    }
 
     Product product =
         productRepository.save(
@@ -97,13 +104,21 @@ public class ProductService {
 
   @Transactional
   public void updateProduct(
-      CustomUserDetails userDetails, UpdateRequestDto updateRequestDto, UUID productId) {
+      CustomUserDetails userDetails, UpdateRequestDto updateRequestDto, MultipartFile file,
+      UUID productId) {
 
     User user = findUserById(userDetails.getUser().getId());
 
     Product product = findProductById(productId);
 
     storeService.checkRoleWithStore(user, product.getStore());
+
+    if (!file.isEmpty()) {
+      s3Service.removeFile(product.getImageUrl());
+      updateRequestDto.setImageUrl(s3Service.uploadFile(file));
+    } else {
+      updateRequestDto.setImageUrl(product.getImageUrl());
+    }
 
     product.update(updateRequestDto);
   }
@@ -123,6 +138,8 @@ public class ProductService {
     Product product = findProductById(productId);
 
     storeService.checkRoleWithStore(user, product.getStore());
+
+    s3Service.removeFile(product.getImageUrl());
 
     product.delete(user);
   }
