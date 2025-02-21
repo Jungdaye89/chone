7a99494d5dae5760592181cfe53488ecee982a3d
KIM SOOBIN <xpsxm225@naver.com>
2025-02-21 21:29:45 +0900

    Feat: 모의 PG Service 클래스 정의
    - 취소 모의 응답 반환
    
    Resolved #85

--- src/main/java/com/chone/server/domains/payment/infrastructure/pg/PgApiService.java
+++ src/main/java/com/chone/server/domains/payment/infrastructure/pg/PgApiService.java
@@ -7,14 +7,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PgApiService {
+
   public Map<String, String> requestPayment() {
     return new MockPgController().processPayment();
   }
 
+  public Map<String, String> requestCancel() {
+    return new MockPgController().processCancel();
+  }
+
   class MockPgController {
+
     public Map<String, String> processPayment() {
-      //      boolean isSuccess = Math.random() > 0.2;
-      boolean isSuccess = false;
+      boolean isSuccess = true;
       String status = isSuccess ? "success" : "fail";
       String message =
           "저희 서비스의 %s로 결제가 %s했습니다."
@@ -26,6 +31,20 @@ public class PgApiService {
       if (isSuccess) {
         response.put("transactionId", RandomStringUtils.randomAlphabetic(10));
       }
+      return response;
+    }
+
+    public Map<String, String> processCancel() {
+      boolean isSuccess = false;
+      String status = isSuccess ? "success" : "fail";
+      String message =
+          "저희 서비스의 %s로 결제 취소가 %s했습니다."
+              .formatted(isSuccess ? "정상 작동" : "오류", isSuccess ? "성공" : "실패");
+
+      Map<String, String> response = new HashMap<>();
+      response.put("status", status);
+      response.put("message", message);
+
       return response;
     }
   }
