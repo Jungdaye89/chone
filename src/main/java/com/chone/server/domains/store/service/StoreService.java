228613e293c389376e9badb2b23ffb8cee04e09c
Goldbar97 <ehrm1357@gmail.com>
2025-02-25 02:30:29 +0900

    Update: 응답 자료형을 DTO 로 변경

--- src/main/java/com/chone/server/domains/store/service/StoreService.java
+++ src/main/java/com/chone/server/domains/store/service/StoreService.java
@@ -90,13 +90,14 @@ public class StoreService {
       String dong,
       Long userId) {
 
-    Page<Store> stores =
+    Page<ReadResponseDto> stores =
         storeRepository.searchStores(
-            page, size, sort, direction, startDate, endDate, category, sido, sigungu, dong, userId);
+            page, size, sort, direction, startDate, endDate, category, sido, sigungu, dong,
+            userId);
 
     return SearchResponseDto.builder()
         .content(
-            stores.getContent().stream().map(ReadResponseDto::from).collect(Collectors.toList()))
+            stores.getContent())
         .pageInfo(
             PageInfoDto.builder()
                 .page(stores.getNumber())
@@ -112,7 +113,7 @@ public class StoreService {
 
     Store store = storeFacade.findStoreById(storeId);
 
-    return ReadResponseDto.from(store);
+    return ReadResponseDto.from(store, 0.0);
   }
 
   @Transactional
