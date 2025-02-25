02f5ec2b2aa2aeaa3139e42db4bb47e037316518
Goldbar97 <ehrm1357@gmail.com>
2025-02-25 12:16:03 +0900

    Update: User Lazy fetch 추가

--- src/main/java/com/chone/server/domains/user/domain/User.java
+++ src/main/java/com/chone/server/domains/user/domain/User.java
@@ -6,13 +6,23 @@ import com.chone.server.commons.jpa.BaseEntity;
 import com.chone.server.domains.order.domain.Order;
 import com.chone.server.domains.review.domain.Review;
 import com.chone.server.domains.store.domain.Store;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Builder.Default;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
@@ -58,17 +68,21 @@ public class User extends BaseEntity {
   @Comment("휴면상태 여부")
   private Boolean isAvailable;
 
-  @OneToMany(mappedBy = "user")
+  @Default
+  @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
   private List<Order> order = new ArrayList<>();
 
-  @OneToMany(mappedBy = "user")
+  @Default
+  @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
   private List<Store> store = new ArrayList<>();
 
-  @OneToMany(mappedBy = "user")
+  @Default
+  @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
   private List<Review> review = new ArrayList<>();
 
   public static UserBuilder builder(
       String username, String email, String hashedPassword, Role role, Boolean isAvailable) {
+
     return User.innerBuilder()
         .username(username)
         .email(email)
@@ -78,15 +92,22 @@ public class User extends BaseEntity {
   }
 
   public void updateIsAvailable() {
+
     this.isAvailable = !this.isAvailable;
   }
 
   public void updateRole(Role role) {
+
     this.role = role;
   }
 
   public void updateUser(String email, String password) {
-    if (hasText(email)) this.email = email;
-    if (hasText(password)) this.password = password;
+
+    if (hasText(email)) {
+      this.email = email;
+    }
+    if (hasText(password)) {
+      this.password = password;
+    }
   }
 }
